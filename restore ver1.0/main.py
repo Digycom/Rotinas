998b0645f96c358f358986fcf9ef172fb134a2fb
Brunno Alberto <brunnoalbertokr@gmail.com>
2018-12-26 12:48:31 -0200

    Correção no código.
    Alterações e finalização. Fase de testes.

--- restore ver1.0/main.py
+++ restore ver1.0/main.py
@@ -7,19 +7,16 @@ import datetime
 from xml.dom import minidom
 import time
 
-default_pwd = "159357"
-reset_pwd = " "
-
-def create_connection(pwd):
+def create_connection():
     try:
-        conn = psycopg2.connect(host="localhost", database="", port="5432", user="postgres", password=pwd)
+        conn = psycopg2.connect(host="localhost", database="", port="5432", user="postgres", password="159357")
     except:
         print("Falha ao conectar com o database")
     return conn
 
 def drop_database(create_connection, dbname):
     print("Deletando o banco de dados " + dbname + "...")
-    conn = create_connection(default_pwd)
+    conn = create_connection()
     
     query = """DROP DATABASE IF EXISTS """ + dbname + """;"""
     conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
@@ -30,7 +27,7 @@ def drop_database(create_connection, dbname):
 
 def create_database(create_connection, dbname):
     print("Criando o banco de dados " + dbname + "...")
-    conn = create_connection(default_pwd)
+    conn = create_connection()
     
     conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     query = """CREATE DATABASE """ + dbname + """ WITH ENCODING='UTF8' OWNER=postgres;"""
@@ -41,7 +38,7 @@ def create_database(create_connection, dbname):
     
 def restore_database(dbname, backup_file_name):
     print("Restaurando o backup do arquivo " + backup_file_name + "...")
-    cmd = """pg_restore.exe --host localhost --port 5432 --username postgres --dbname """ + dbname + """ """ + backup_file_name
+    cmd = """pg_restore.exe --host localhost --port 5432 --username postgres --dbname """ + dbname + """ ./bkps/""" + backup_file_name
     returned_value = os.system(cmd)
     print('returned value:', returned_value)
 
@@ -53,24 +50,6 @@ def prepare_backup_name(dbname):
     name = dbname + "__" + year + "_" + month + "_" + day + ".backup"
 
     return name
-
-def set_passwd_null(create_connection):
-    conn = create_connection(default_pwd)
-    
-    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    query = """ALTER USER postgres WITH PASSWORD ' ';"""
-    cur = conn.cursor()
-    cur.execute(query)
-    conn.close()
-
-def reset_passwd(create_connection):
-    conn = create_connection(reset_pwd)
-    
-    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    query = """ALTER USER postgres WITH PASSWORD '159357';"""
-    cur = conn.cursor()
-    cur.execute(query)
-    conn.close()
 
 print("Iniciando o processo")
 
@@ -91,9 +70,7 @@ for elem in dbname:
     # Iniciando as rotinas
     drop_database(create_connection, dbname)
     create_database(create_connection, dbname)
-    set_passwd_null(create_connection)
     restore_database(dbname, backup_file_name)
-    reset_passwd(create_connection)
 
     end = time.time()
 
